5c9186bc7de209e3ff5ee69acee7fdbcd46d61bb
Naum Soloveychik <naum@MacBook-Pro-Naum-2.local>
2020-01-12 13:05:42 +0300

    ignore tables mysql.proc and mysql.slow_log

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -107,7 +107,8 @@ mysqlparams="   --all-databases \
                 --comments \
                 --quick \
                 --force \
-#                --ignore-table=mysql.proc \
+                --ignore-table=mysql.proc \
+                --ignore-table=mysql.slow_log \
                 --log-error=/var/log/mysqldump.log" ;
 
 if [ "${singletrans}" ] ; then
